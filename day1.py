1b8364976767216ef2d6b5c2fa462f013024388e
xct-r3v3rse <xct@vulndev.io>
2019-12-03 09:45:04 +0100

    indent

--- day1.py
+++ day1.py
@@ -9,32 +9,32 @@ Specifically, to find the fuel required for a module, take its mass, divide by t
 
 data = []
 with open('data/day1.txt','r') as f:
-	data = f.readlines()
+    data = f.readlines()
 
 # Part 1 (Fuel for the base mass)
 sum1 = 0
 for mass in data:
-	mass = int(mass)
-	fuel = mass//3 - 2
-	sum1 += fuel
+    mass = int(mass)
+    fuel = mass//3 - 2
+    sum1 += fuel
 print(f'Sum = {sum1}')
 # That's the right answer! You are one gold star closer to rescuing Santa.
 
 # Part 2 (Fuel for the fuel)
-def get_fuel(mass):	
-	fuel_sum = 0
-	remaining_mass = mass
-	while True:		
-		fuel = remaining_mass//3 - 2
-		if fuel <= 0:
-			break
-		fuel_sum += fuel
-		remaining_mass = fuel
-	return fuel_sum
+def get_fuel(mass): 
+    fuel_sum = 0
+    remaining_mass = mass
+    while True:     
+        fuel = remaining_mass//3 - 2
+        if fuel <= 0:
+            break
+        fuel_sum += fuel
+        remaining_mass = fuel
+    return fuel_sum
 
 sum2 = 0
 for mass in data:
-	mass = int(mass)
-	fuel = get_fuel(mass)
-	sum2 += fuel
+    mass = int(mass)
+    fuel = get_fuel(mass)
+    sum2 += fuel
 print(f'Sum = {sum2}')
--- day3.py
+++ day3.py
@@ -12,66 +12,66 @@ np.core.arrayprint._line_width = 180
 #p2 = ['U98','R91','D20','R16','D67','R40','U7','R15','U6','R7']
 
 with open('data/day3_p1.txt', 'r') as f:
-	p1 = f.read().split(',')
+    p1 = f.read().split(',')
 with open('data/day3_p2.txt', 'r') as f:
-	p2 = f.read().split(',')
+    p2 = f.read().split(',')
 
 def grid_move_steps(point, direction, distance, marker, steps):
-	p = [point[0],point[1]]
-	print(p)
-	if direction == 'R':
-		for i in range(distance):			
-			x = p[0]
-			y = p[1]
-			if grid[x,y] == 0:
-				grid[x,y] = marker
-			elif grid[x,y] != marker:
-				grid[x,y] = 3
-				if (x,y) in step_map:
-					step_map[(x,y)] += steps + i
-				else:
-				 	step_map[(x,y)] = steps + i
-			p[0] += 1	
-	elif direction == 'L':
-		for i in range(distance):			
-			x = p[0]
-			y = p[1]
-			if grid[x,y] == 0:
-				grid[x,y] = marker
-			elif grid[x,y] != marker:
-				grid[x,y] = 3
-				if (x,y) in step_map:
-					step_map[(x,y)] += steps + i
-				else:
-				 	step_map[(x,y)] = steps + i
-			p[0] -= 1	
-	elif direction == 'U':
-		for i in range(distance):			
-			x = p[0]
-			y = p[1]
-			if grid[x,y] == 0:
-				grid[x,y] = marker
-			elif grid[x,y] != marker:
-				grid[x,y] = 3	
-				if (x,y) in step_map:
-					step_map[(x,y)] += steps + i
-				else:
-				 	step_map[(x,y)] = steps + i
-			p[1] += 1	
-	elif direction == 'D':
-		for i in range(distance):			
-			x = p[0]
-			y = p[1]
-			if grid[x,y] == 0:
-				grid[x,y] = marker
-			elif grid[x,y] != marker:
-				grid[x,y] = 3
-				if (x,y) in step_map:
-					step_map[(x,y)] += steps + i
-				else:
-				 	step_map[(x,y)] = steps + i
-			p[1] -= 1		
-	return p, steps+distance
+    p = [point[0],point[1]]
+    print(p)
+    if direction == 'R':
+        for i in range(distance):           
+            x = p[0]
+            y = p[1]
+            if grid[x,y] == 0:
+                grid[x,y] = marker
+            elif grid[x,y] != marker:
+                grid[x,y] = 3
+                if (x,y) in step_map:
+                    step_map[(x,y)] += steps + i
+                else:
+                    step_map[(x,y)] = steps + i
+            p[0] += 1   
+    elif direction == 'L':
+        for i in range(distance):           
+            x = p[0]
+            y = p[1]
+            if grid[x,y] == 0:
+                grid[x,y] = marker
+            elif grid[x,y] != marker:
+                grid[x,y] = 3
+                if (x,y) in step_map:
+                    step_map[(x,y)] += steps + i
+                else:
+                    step_map[(x,y)] = steps + i
+            p[0] -= 1   
+    elif direction == 'U':
+        for i in range(distance):           
+            x = p[0]
+            y = p[1]
+            if grid[x,y] == 0:
+                grid[x,y] = marker
+            elif grid[x,y] != marker:
+                grid[x,y] = 3   
+                if (x,y) in step_map:
+                    step_map[(x,y)] += steps + i
+                else:
+                    step_map[(x,y)] = steps + i
+            p[1] += 1   
+    elif direction == 'D':
+        for i in range(distance):           
+            x = p[0]
+            y = p[1]
+            if grid[x,y] == 0:
+                grid[x,y] = marker
+            elif grid[x,y] != marker:
+                grid[x,y] = 3
+                if (x,y) in step_map:
+                    step_map[(x,y)] += steps + i
+                else:
+                    step_map[(x,y)] = steps + i
+            p[1] -= 1       
+    return p, steps+distance
 
 
 #origin = [0,0]
@@ -83,24 +83,24 @@ step_map = {}
 
 cur_steps = 0
 p = origin
-for i in range(len(p1)):	
-	print(p1[i])
-	p, cur_steps = grid_move_steps(p, p1[i][0], int(p1[i][1:]), 1, cur_steps)
+for i in range(len(p1)):    
+    print(p1[i])
+    p, cur_steps = grid_move_steps(p, p1[i][0], int(p1[i][1:]), 1, cur_steps)
 cur_steps = 0
 p = origin
 for i in range(len(p2)):
-	print(p2[i])
-	p, cur_steps = grid_move_steps(p, p2[i][0], int(p2[i][1:]), 2, cur_steps)
+    print(p2[i])
+    p, cur_steps = grid_move_steps(p, p2[i][0], int(p2[i][1:]), 2, cur_steps)
 
 min_dist = 9999999
 min_coords = None
 crosses = np.where(grid == 3)
 crosses = list(zip(crosses[0], crosses[1]))
 for i, cross in enumerate(crosses):
-	dist = np.abs(origin[0] - cross[0]) + np.abs(origin[1]-cross[1])
-	if dist != 0 and dist < min_dist:
-		min_dist = dist
-		min_coords = cross
+    dist = np.abs(origin[0] - cross[0]) + np.abs(origin[1]-cross[1])
+    if dist != 0 and dist < min_dist:
+        min_dist = dist
+        min_coords = cross
 print(f'Minimum dist: {min_dist} / {min_coords}') # 1017
 
 # = 2nd pass = 
@@ -109,23 +109,23 @@ grid = np.zeros(shape=(40000,40000))
 cur_steps = 0
 p = origin
 for i in range(len(p2)):
-	print(p2[i])
-	p, cur_steps = grid_move_steps(p, p2[i][0], int(p2[i][1:]), 2, cur_steps)
+    print(p2[i])
+    p, cur_steps = grid_move_steps(p, p2[i][0], int(p2[i][1:]), 2, cur_steps)
 cur_steps = 0
 p = origin
-for i in range(len(p1)):	
-	print(p1[i])
-	p, cur_steps = grid_move_steps(p, p1[i][0], int(p1[i][1:]), 1, cur_steps)
+for i in range(len(p1)):    
+    print(p1[i])
+    p, cur_steps = grid_move_steps(p, p1[i][0], int(p1[i][1:]), 1, cur_steps)
 
 min_dist = 9999999
 min_coords = None
 crosses = np.where(grid == 3)
 crosses = list(zip(crosses[0], crosses[1]))
 for i, cross in enumerate(crosses):
-	dist = np.abs(origin[0] - cross[0]) + np.abs(origin[1]-cross[1])
-	if dist != 0 and dist < min_dist:
-		min_dist = dist
-		min_coords = cross
+    dist = np.abs(origin[0] - cross[0]) + np.abs(origin[1]-cross[1])
+    if dist != 0 and dist < min_dist:
+        min_dist = dist
+        min_coords = cross
 print(f'Min dist: {min_dist} / {min_coords}') # 1017
 
 # = aggregate results =
